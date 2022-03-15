80f25d9e82b012bd7046a18a245a0a19b49e634f
AndresCeronC <aceron@cyd-tec.com>
2022-03-15 18:05:20 -0300

    FIX: Add CORS para poder trabajar con React/Vue

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
 const http = require('http');
+// Se incluye para poder hacer request desde otras rutas.
+const cors = require('cors');
 
 // Setting express
 const app = express();
@@ -11,7 +12,9 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+// Permite las consultas desde Vue / React
+// noinspection JSCheckFunctionSignatures
+app.use( cors({ origin: true, credentials: true  }) );
 // Agregando rutas.
 require('./routes')(app);
 
